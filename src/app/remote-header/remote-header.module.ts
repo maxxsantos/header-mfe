365beafa38b1a5f3431c7f710e950663c54c87ad
Max <max.santos@icomp.ufam.edu.br>
2022-03-03 20:42:00 -0400

    header

---
+++ src/app/remote-header/remote-header.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HeaderComponent,
+      },
+    ]),
+  ]
+})
+export class RemoteHeaderModule { }
